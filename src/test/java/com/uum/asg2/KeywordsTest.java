93693b2fc09d132cce48903e94a0bea43188e775
Mei Yit <Mei Yit@10.31.106.170>
2017-10-30 01:05:23 +0800

    Revert ""
    This reverts commit ca55fc6b545988edd10ac78928d62a0e0d52bc46.

---
+++ src/test/java/com/uum/asg2/KeywordsTest.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.uum.asg2;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Mei Yit
+ */
+public class KeywordsTest {
+
+    public KeywordsTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of keywords method, of class keywords.
+     */
+    @Test
+    public void testKeywords() throws Exception {
+        System.out.println("keywords");
+        int[] expResult = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+                                    0, 1, 1, 0, 0, 2, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0};
+
+        int[] result = Keywords.keywords();
+        assertArrayEquals(expResult, result);
+
+    }
+
+}
