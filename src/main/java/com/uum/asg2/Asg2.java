71506e994569e43d3e96d40117173045e1f09161
TeeMeiYit <31920062+TeeMeiYit@users.noreply.github.com>
2017-11-01 19:11:51 +0800

    Update Asg2.java

--- src/main/java/com/uum/asg2/Asg2.java
+++ src/main/java/com/uum/asg2/Asg2.java
@@ -12,19 +12,28 @@ import static com.uum.asg2.Cloc.cloc;
 import static com.uum.asg2.Cloc.comment;
 import static com.uum.asg2.CommentInfo.commentInfo;
 import static com.uum.asg2.Keywords.keywords;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
 
 public class Asg2 {
 
     // public static final String directory = "C:\\Users\\Mei Yit\\Documents\\NetBeansProjects\\240788_A2\\test\\MyThread1.java";
-    static String filename = "test\\MyThread1.java";
+    static String filename = "test";
     static String workingDirectory = System.getProperty("user.dir");
     static String absoluteFilePath = workingDirectory + File.separator + filename;
+    static ArrayList javafile = new ArrayList();
+    static File directory = new File(absoluteFilePath);
+    static File[] files = directory.listFiles();
 
     public static void main(String args[]) throws FileNotFoundException, IOException {
 
+        System.out.println("Directory path : " + absoluteFilePath);
         int[] keywordsCount;
         String[] Keywords = {"abstract", "assert", "boolean", "break", "byte", "case",
             "catch", "char", "class", "continue", "default", "do", "double", "else",
@@ -36,16 +45,27 @@ public class Asg2 {
 
         String courseInfo[];
 
-        if (absoluteFilePath != null) {
-            courseInfo = commentInfo();
-            int lineNumberCount = cloc();
-            int blankCount = blank();
-            int commentCount = comment();
-            int actual = actualLOC();
-            keywordsCount = keywords();
-            int loc[] = {lineNumberCount, blankCount, commentCount, actual};
-            Excel ex = new Excel();
-            ex.exportResultInExcel(courseInfo, loc, Keywords, keywordsCount);
+        for (File fileName : files) {
+                if (fileName.getName().endsWith(".java")) {
+                    for (int i = 0; i < javafile.size(); i++) {
+                        absoluteFilePath = javafile.get(i).toString();
+                        try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(absoluteFilePath), Charset.defaultCharset()))) {
+                            courseInfo = commentInfo();
+                            int lineNumberCount = cloc();
+                            int blankCount = blank();
+                            int commentCount = comment();
+                            int actual = actualLOC();
+                            keywordsCount = keywords();
+                            int loc[] = {lineNumberCount, blankCount, commentCount, actual};
+                            Excel ex = new Excel();
+                            ex.exportResultInExcel(courseInfo, loc, Keywords, keywordsCount);
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                javafile.add(fileName.getAbsolutePath());
+            } 
         }
     }
 }
